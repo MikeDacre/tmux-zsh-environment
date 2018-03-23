ce25df82fef05f7f21d35345fcbfc0d3f1b9e505
Mike Dacre <mike.dacre@gmail.com>
2018-03-23 15:18:52 -0700

    Use the -s option instead

--- tmux-zsh-environment.plugin.zsh
+++ tmux-zsh-environment.plugin.zsh
@@ -10,18 +10,7 @@
 if [ -n "$TMUX" ] && tmux ls >/dev/null 2>/dev/null; then
     # Update shell environment from variables
     _tmux_zsh_env__precmd() {
-        local envs
-        local old_ifs
-        # Split on newline only
-        old_ifs=${IFS}
-        # Get only set variables from tmux
-        IFS=$'\n' envs=($(tmux show-environment | grep -v "^-" | grep '=' | sed 's/ /\\ /g'))
-        # Reset IFS so we don't break other things
-        IFS=${old_ifs}
-        # Evaluate
-        for env in "${envs[@]}"; do
-            eval "${env}"
-        done
+        eval "${tmux show-environment -s}"
     }
 
     # Add the function as a precmd, to be run at every new prompt
