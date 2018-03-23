35ae7862aa85bad07eb9b50b97e8303b6787f486
Mike Dacre <mike.dacre@gmail.com>
2018-03-23 14:55:45 -0700

    Only run if in a tmux session
    Don't do anything else if tmux session is not attached, also set
    TMUX_ZSH_ENVIRONMENT_SYNCED to allow other plugins to detect activity.

--- tmux-zsh-environment.plugin.zsh
+++ tmux-zsh-environment.plugin.zsh
@@ -3,25 +3,28 @@
 #                    Tmux ZSH Environment Sharing                     #
 #######################################################################
 
-# Update shell environment from variables
-_tmux_zsh_env__precmd() {
-    local envs
-    local old_ifs
-    # Split on newline only
-    old_ifs=${IFS}
-    # Get only set variables from tmux
-    IFS=$'\n' envs=($(tmux show-environment | grep -v "^-" | grep '=' | sed 's/ /\\ /g'))
-    IFS=${old_ifs}
-    # Evaluate
-    for env in "${envs[@]}"; do
-        eval "${env}"
-    done
-}
+# Do nothing at all if we are not in tmux, second test is needed in
+# case of an ssh session being run from within tmux, which could result
+# in $TMUX from the parent session being set without tmux being
+# connected.
+if [ -n "$TMUX" ] && tmux ls >/dev/null 2>/dev/null; then
+    # Update shell environment from variables
+    _tmux_zsh_env__precmd() {
+        local envs
+        local old_ifs
+        # Split on newline only
+        old_ifs=${IFS}
+        # Get only set variables from tmux
+        IFS=$'\n' envs=($(tmux show-environment | grep -v "^-" | grep '=' | sed 's/ /\\ /g'))
+        # Reset IFS so we don't break other things
+        IFS=${old_ifs}
+        # Evaluate
+        for env in "${envs[@]}"; do
+            eval "${env}"
+        done
+    }
 
-# Add the function as a precmd, so it runs on every prompt update
-# Only do this if no one has set TMUX_ENV_AUTOUPDATE_DONE, this allows
-# other plugins to specify this without worrying about duplicate work.
-if [ -z "$TMUX_ENV_AUTOUPDATE_DONE" ]; then
+    # Add the function as a precmd, to be run at every new prompt
     autoload -Uz add-zsh-hook
     add-zsh-hook precmd _zsh_title__precmd
 fi
