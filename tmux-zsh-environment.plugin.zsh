a4b8acc3fbfcd3db1e5510216bb8384a858e5479
Mike Dacre <mike.dacre@gmail.com>
2018-03-23 15:49:29 -0700

    Allow disabling by variable

--- tmux-zsh-environment.plugin.zsh
+++ tmux-zsh-environment.plugin.zsh
@@ -3,6 +3,20 @@
 #                    Tmux ZSH Environment Sharing                     #
 #######################################################################
 
+# Don't run if @delphij's version is running
+[[ -n "$ZSH_TMUX_AUTOREFRESH" ]] || ZSH_TMUX_AUTOREFRESH=false
+
+if "$ZSH_TMUX_AUTOREFRESH"; then
+    return
+fi
+
+# Control running with TMUX_ZSH_SYNC
+[[ -n "$TMUX_ZSH_SYNC" ]] || TMUX_ZSH_SYNC=true
+
+if ! "$TMUX_ZSH_SYNC"; then
+    return
+fi
+
 # Do nothing at all if we are not in tmux, second test is needed in
 # case of an ssh session being run from within tmux, which could result
 # in $TMUX from the parent session being set without tmux being
